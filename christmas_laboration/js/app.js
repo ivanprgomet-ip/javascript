38f738945667c4e93aef8d04255cd62df929fdcf
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-27 23:48:32 +0100

    movies and users arrays in a factory. displaying movies with ng-repeat in div

--- christmas_laboration/js/app.js
+++ christmas_laboration/js/app.js
@@ -1,21 +1,38 @@
 ﻿/// <reference path="angular.min.js" />
 
-var users = [
-    { name: "ivan prgomet", username: "ivanprgomet", password: "ivan123" },
-    { name: "lea winchester", username: "leawinchester", password: "leawin" },
-    { name: "jason bourne", username: "jasonbourne", password: "bourne" },
-    { name: "duncan belaius", username: "duncanbelaius", password: "duncan123" },
-    { name: "bella marbles", username: "bellamarbles", password: "chailatte" },
-]
-var movies= [
-    { title: "the dark knight", year: "2010" },
-    { title: "the shawshank redemption", year: "1994" },
-    { title: "the godfather", year: "1972" },
-    { title: "schinlders list", year: "1993" },
-    { title: "pulp fiction", year: "1994" },
-    { title: "lord of the rings", year: "2003" },
-    { title: "fight club", year: "1999" },
-    { title: "forrest gump", year: "1994" },
-    { title: "inception", year: "2010" },
-    { title: "matrix", year: "1999" },
-]
+var app = angular.module("moviesApp", []);
+var homecontroller = function ($scope,moviesFactory) {
+    $scope.hero = "welcome to showtime movies"
+    $scope.movies = moviesFactory.movies;
+}
+
+app.factory("moviesFactory", function () {
+    //factory content
+    var myFactory = {};
+
+    myFactory.users = [
+        { name: "ivan prgomet", username: "ivanprgomet", password: "ivan123" },
+        { name: "lea winchester", username: "leawinchester", password: "leawin" },
+        { name: "jason bourne", username: "jasonbourne", password: "bourne" },
+        { name: "duncan belaius", username: "duncanbelaius", password: "duncan123" },
+        { name: "bella marbles", username: "bellamarbles", password: "chailatte" },
+    ]
+    myFactory.movies = [
+        { title: "the dark knight", year: "2010" },
+        { title: "the shawshank redemption", year: "1994" },
+        { title: "the godfather", year: "1972" },
+        { title: "schinlders list", year: "1993" },
+        { title: "pulp fiction", year: "1994" },
+        { title: "lord of the rings", year: "2003" },
+        { title: "fight club", year: "1999" },
+        { title: "forrest gump", year: "1994" },
+        { title: "inception", year: "2010" },
+        { title: "matrix", year: "1999" },
+    ]
+
+    return myFactory;
+})
+
+app.controller("homecontroller", homecontroller);//register controller
+
+
