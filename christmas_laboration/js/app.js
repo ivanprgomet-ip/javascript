738676ad2c74df8f15d2570c06cd40f39603de48
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-27 23:01:52 +0100

    created arrays

---
+++ christmas_laboration/js/app.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="angular.min.js" />
+
+var users = [
+    { name: "ivan prgomet", username: "ivanprgomet", password: "ivan123" },
+    { name: "lea winchester", username: "leawinchester", password: "leawin" },
+    { name: "jason bourne", username: "jasonbourne", password: "bourne" },
+    { name: "duncan belaius", username: "duncanbelaius", password: "duncan123" },
+    { name: "bella marbles", username: "bellamarbles", password: "chailatte" },
+]
+var movies= [
+    { title: "the dark knight", year: "2010" },
+    { title: "the shawshank redemption", year: "1994" },
+    { title: "the godfather", year: "1972" },
+    { title: "schinlders list", year: "1993" },
+    { title: "pulp fiction", year: "1994" },
+    { title: "lord of the rings", year: "2003" },
+    { title: "fight club", year: "1999" },
+    { title: "forrest gump", year: "1994" },
+    { title: "inception", year: "2010" },
+    { title: "matrix", year: "1999" },
+]
