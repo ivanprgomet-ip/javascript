fb8235a38f9b72bba755b936f412096d79ef57eb
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-03 01:09:43 +0100

    lab6.1 routing and custom html done

---
+++ lesson6/lab6.1-routing_with_custom_html/js/routing.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular.module("fruitsApp", ["ngRoute"]); //telling angular that i have an dependenciy on ngRoute
+
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            template:
+                "<a href='#/bananas'>bananas</a><br/>"+
+                "<a href='#/apples'>apples</a><br/>" +
+                "<a href='#/pears'>pears</a><br/>" +
+                "<a href='#/pineapples'>pineapples</a><br/>" +
+                "<a href='#/coconuts'>coconuts</a><br/>"
+        })
+        .when("/bananas", { template: "bananas are for monkeys and dietists" })
+        .when("/apples", { template: "one apple a day keeps the doctor away" })
+        .when("/pears", { template: "pears are the cousins of apples" })
+        .when("/pineapples", { template: "pineapples are the exact same as apples, but with a pine in the name" })
+        .when("/coconuts", { template: "coconuts are nuts inlove with the coco" })
+
+        .otherwise({redirectTo:"/"})
+})
+
