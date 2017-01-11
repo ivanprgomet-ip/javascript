ea6a02d53c1528b8018cf4e4a51e15d01939b6aa
Ivan Prgomet <ivan_prgomet@hotmail.com>
2017-01-11 09:42:11 +0100

    searching for persons, also showing how many results found etc. using jquery

--- repetition/one_arrays_loops_namespaces/one_arrays_loops_namespaces/scripts/persons.js
+++ repetition/one_arrays_loops_namespaces/one_arrays_loops_namespaces/scripts/persons.js
@@ -28,7 +28,10 @@ firstApp.showAll = $("#show-all");
 firstApp.showSearch = $("#show-search");
 firstApp.showSave = $("#show-save");
 
-firstApp.showAll.click(function () {
+firstApp.search = $("#search");
+firstApp.recentSearch = $("#recent-search");
+
+firstApp.showAll.click(function (e) {
     $("#search-panel").hide();
     $("#save-panel").hide();
     $("#persons-output").toggle();
@@ -44,6 +47,39 @@ firstApp.showSearch.click(function (e) {
     $("#persons-output").hide();
     $("#save-panel").hide();
     $("#search-panel").toggle();
+})
+
+firstApp.search.on("click", function (e) {
+    /*difference between jquery.html (= innerHTML) and jquery.append (+= innerHTML*/
+
+    $("#searchResults").html("")
+    $("#searchResultsCount").removeClass("NothingFoundRed");
+    var resultCount = 0;
+
+    var searchvalue = $("#searchvalue").val();
+
+    $.each(firstApp.persons, function (index, value) {
+        console.log("inside search loop" + value.firstname)
+        if (searchvalue.includes(value.firstname) ||
+            searchvalue.includes(value.lastname) ||
+            searchvalue.includes(value.firstname + " " + value.lastname)) {
+            resultCount += 1;
+            $("#searchResults").append(value.firstname + " " + value.lastname + " (" + value.age + ") " + value.city + "<br/>");
+        }
+    })
+    if (resultCount > 0)
+        $("#searchResultsCount").html(resultCount + " person(s) found");
+    else {
+        $("#searchResultsCount").addClass("NothingFoundRed");
+        $("#searchResultsCount").html("No results found");
+    }
+
+    localStorage.setItem("MostRecentSearch", searchvalue);
+})
+
+firstApp.recentSearch.on("click", function (e) {
+    var mostRecentSearch = localStorage.getItem("MostRecentSearch");
+    $("#searchvalue").val(mostRecentSearch);
 })
 
 
