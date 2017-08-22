418e5303f2f2e2d48d1fc34bc11f8932fa219f20
ivanprgomet-ip <ivan_prgomet@hotmail.com>
2017-08-22 10:18:55 +0200

    preparations

--- lab5.1-angular_factories_and_controllers/myapp.js
+++ lab5.1-angular_factories_and_controllers/myapp.js
@@ -1,67 +1,126 @@
-﻿/// <reference path="angular.min.js" />
+﻿var App = angular.module("App", []);
 
-var app = angular.module("myapp", []);
+//Angular Factory
+App.factory("schoolFactory", function () {
 
-//create homecontroller
-//passing in educationFactory by its name to access its methods and variables
-var HomeController = function ($scope, educationFactory) {
-
-    //when we call getstudents, the factory gives 
-    //all the students into the scope variable, which 
-    //then gets visible on the html page, where the current 
-    //controller is ruling. and we iterate the $scope.students
-    //using the ng-repeat
-    $scope.GetStudents = function () {
-        $scope.students = educationFactory.getStudents();
-    };
-    $scope.GetEducations = function () {
-        $scope.educations = educationFactory.getEducations();
-    }
-}
-
-//creating angular factory for the module
-app.factory("educationFactory", function () {
-    //placing all data inside factory that we consider relevant for it
-    var students = [
-        { studentName: "ivan prgomet", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "lea winchester", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "bob hudson", isActive: true, education: "test driven development" },
-        { studentName: "ken lewis", isActive: true, education: "test driven development" },
-        { studentName: "frank dupont", isActive: true, education: "test driven development" },
-        { studentName: "abrahamn bond", isActive: true, education: "network security" },
-        { studentName: "james bond", isActive: true, education: "network security" },
-        { studentName: "cindy lauper", isActive: true, education: "network security" },
-        { studentName: "sara bergerson", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "frankie federossi", isActive: false, education: "webdevelopment within .NET" },
-        { studentName: "benjamin franciné", isActive: true, education: "network security" },
-        { studentName: "luca toni", isActive: true, education: "linux programming" },
-        { studentName: "bob kelso", isActive: true, education: "linux programming" },
-        { studentName: "sam smith", isActive: false, education: "test driven development" },
-        { studentName: "kenny rogers", isActive: true, education: "frontend development" },
-    ]
     var educations = [
-        { educationName: "webdevelopment within .NET" },
-        { educationName: "test driven development" },
-        { educationName: "network security" },
-        { educationName: "linux programming" },
-        { educationName: "frontend development" },
+        {
+            name: "Webdeveloper within .NET"
+        },
+        {
+            name: "Linux Shell Programming"
+        },
+        {
+            name: "Frontend Development"
+        },
+        {
+            name: "C#"
+        },
+        {
+            name: "Javascript and Angular JS"
+        },
+
+    ];
+
+    var persons = [
+        {
+            name: "Thomas",
+            status: false,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Polly",
+            status: true,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Ada",
+            status: true,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Frank",
+            status: false,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Grace",
+            status: true,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Finn",
+            status: true,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Bobby",
+            status: false,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Billy",
+            status: true,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Tommy",
+            status: true,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Johnny",
+            status: false,
+            utbildning: "C#"
+        },
+        {
+            name: "Karl",
+            status: true,
+            utbildning: "C#"
+        },
+        {
+            name: "Ivan",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Serena",
+            status: false,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Selena",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Mike",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
+
+
+
     ]
 
-    //creating array of factory related stuff
-    var myFactory = {};
+    var factory = {};
 
-    myFactory.getStudents = function () {
-        return students;
-    }
-    myFactory.getEducations = function () {
+    factory.getPersons = function () {
+        return persons;
+    };
+
+    factory.getEducations = function () {
         return educations;
-    }
+    };
 
-    //returning the factory that contains all teh factory related stuff
-    return myFactory;
+    return factory;
 });
 
+var controllers = {};
 
+controllers.schoolController = function ($scope, schoolFactory) {
+    $scope.educations = schoolFactory.getEducations();
+    $scope.persons = schoolFactory.getPersons();
+};
 
-//activate/attach homecontroller to the module
-app.controller("HomeController", HomeController);
+App.controller(controllers);
