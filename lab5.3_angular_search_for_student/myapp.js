69cec4b96da34c322064db8b3c3257fd1f8940f6
ivanprgomet-ip <ivan_prgomet@hotmail.com>
2017-08-22 11:19:05 +0200

    preparations

--- lab5.3_angular_search_for_student/myapp.js
+++ lab5.3_angular_search_for_student/myapp.js
@@ -1,107 +1,160 @@
-﻿/// <reference path="angular.min.js" />
+﻿var App = angular.module("App", []);
 
-var app = angular.module("myapp", []);
+//Angular Factory
 
-//create homecontroller
-//passing in educationFactory by its name to access its methods and variables
-var HomeController = function ($scope, educationFactory) {
+App.factory("schoolFactory", function () {
 
-    //when we call getstudents, the factory gives 
-    //all the students into the scope variable, which 
-    //then gets visible on the html page, where the current 
-    //controller is ruling. and we iterate the $scope.students
-    //using the ng-repeat
-    $scope.GetStudents = function () {
-        $scope.students = educationFactory.getStudents();
-    };
-    $scope.GetEducations = function () {
-        $scope.educations = educationFactory.getEducations();
-    }
+    var utbildningar = [
+        {
+            name: "Webdeveloper within .NET"
+        },
+        {
+            name: "Linux Shell Programming"
+        },
+        {
+            name: "Frontend Development"
+        },
+        {
+            name: "C#"
+        },
+        {
+            name: "Javascript and Angular JS"
+        },
 
-    $scope.AddStudent = function () {
-        educationFactory.addStudentToArray({
-            studentName: $scope.studentName,
-            education: $scope.studentEducation
-        });
-    }
+    ];
 
-    $scope.SearchForStudents = function () {
-        console.log($scope.searchedTerm);
-        $scope.FoundStudents = educationFactory.searchForStudent($scope.searchedTerm);
-
-        //most recent search results is saved in localstorage
-        localStorage.setItem("MostRecentSearchResults", $scope.FoundStudents);
-        localStorage.setItem("MostRecentSearchTerm", $scope.searchedTerm);
-    };
-
-    $scope.PopulateInputWithRecentSearch = function () {
-        var mostRecentSearch = localStorage.getItem("MostRecentSearchTerm");
-        $scope.searchedTerm = mostRecentSearch;
-    }
-
-}
-
-//creating angular factory for the module
-app.factory("educationFactory", function () {
-    //placing all data inside factory that we consider relevant for it
-    var students = [
-        { studentName: "ivan prgomet", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "lea winchester", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "bob hudson", isActive: true, education: "test driven development" },
-        { studentName: "ken lewis", isActive: true, education: "test driven development" },
-        { studentName: "frank dupont", isActive: true, education: "test driven development" },
-        { studentName: "abrahamn bond", isActive: true, education: "network security" },
-        { studentName: "james bond", isActive: true, education: "network security" },
-        { studentName: "cindy lauper", isActive: true, education: "network security" },
-        { studentName: "sara bergerson", isActive: true, education: "webdevelopment within .NET" },
-        { studentName: "frankie federossi", isActive: false, education: "webdevelopment within .NET" },
-        { studentName: "benjamin franciné", isActive: true, education: "network security" },
-        { studentName: "luca toni", isActive: true, education: "linux programming" },
-        { studentName: "bob kelso", isActive: true, education: "linux programming" },
-        { studentName: "sam smith", isActive: false, education: "test driven development" },
-        { studentName: "kenny rogers", isActive: true, education: "frontend development" },
+    var persons = [
+        {
+            name: "Thomas",
+            status: false,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Polly",
+            status: true,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Ada",
+            status: true,
+            utbildning: "Webdeveloper within .NET"
+        },
+        {
+            name: "Frank",
+            status: false,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Grace",
+            status: true,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Finn",
+            status: true,
+            utbildning: "Linux Shell Programming"
+        },
+        {
+            name: "Bobby",
+            status: false,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Billy",
+            status: true,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Tommy",
+            status: true,
+            utbildning: "Frontend Development"
+        },
+        {
+            name: "Johnny",
+            status: false,
+            utbildning: "C#"
+        },
+        {
+            name: "Karl",
+            status: true,
+            utbildning: "C#"
+        },
+        {
+            name: "Ivan",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Serena",
+            status: false,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Selena",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
+        {
+            name: "Mike",
+            status: true,
+            utbildning: "Javascript and Angular JS"
+        },
     ]
-    var educations = [
-        { educationName: "webdevelopment within .NET" },
-        { educationName: "test driven development" },
-        { educationName: "network security" },
-        { educationName: "linux programming" },
-        { educationName: "frontend development" },
-    ]
 
-    //creating array of factory related stuff
-    var myFactory = {};
+    var personsSearch = [];
 
-    myFactory.getStudents = function () {
-        return students;
-    }
-    myFactory.getEducations = function () {
-        return educations;
-    }
-    myFactory.addStudentToArray = function (student) {
-        students.push(student);
-    }
+    var factory = {};
 
-    myFactory.searchForStudent = function (searched) {
-        var result = [];
+    factory.Search = function (name) {
+        personsSearch.length = 0;
+        angular.forEach(persons, function (value, key) {
 
-        console.log("inside factory searchforstudent method");
-        console.log("searched parameter inside factory is: " + searched);
-        for (var i = 0; i < students.length; i++) {
-            console.log(students[i].studentName);
-            if (students[i].studentName == searched || students[i].education === searched) {
-                console.log("student retrieved back was :"+students[i]);
-                result.push(students[i]);
+            if (value.name.includes(name)) {
+                personsSearch.push(value);
             }
-        }
-        return result;
-    }
+        });
+    };
 
-    //returning the factory that contains all teh factory related stuff
-    return myFactory;
+    factory.getSearch = function () {
+        return personsSearch;
+    };
+
+    factory.getPersons = function () {
+        return persons;
+    };
+
+    factory.getUtbildningar = function () {
+        return utbildningar;
+    };
+
+    factory.AddPersonToArray = function (person) {
+        persons.push(person);
+    };
+
+    return factory;
 });
 
+var controllers = {};
 
+controllers.schoolController = function ($scope, schoolFactory) {
+    $scope.utbildningar = schoolFactory.getUtbildningar();
+    $scope.persons = schoolFactory.getPersons();
+    $scope.searchResult = schoolFactory.getSearch();
+    $scope.AddPerson = function () {
+        schoolFactory.AddPersonToArray({ name: $scope.newPersonName, utbildning: $scope.newPersonUtbildning, status: $scope.newPersonAktiv })
+    };
 
-//activate/attach homecontroller to the module
-app.controller("HomeController", HomeController);
+    $scope.SearchPerson = function () {
+        schoolFactory.Search($scope.searchPersonName);
+        localStorage.setItem("search", $scope.searchPersonName);
+    };
+
+    $scope.GetLocaLStorage = function () {
+        var string = localStorage.getItem("search");
+        $scope.searchPersonName = string;
+        schoolFactory.Search(string);
+    };
+};
+
+App.controller(controllers);
+
